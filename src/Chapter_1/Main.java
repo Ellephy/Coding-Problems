ba83221f1387b4ee31fd4b31ced79ece1472c613
Ellephy <aungphyopi@gmail.com>
2021-06-06 21:29:31 +0630

    Chap-1 commit

---
+++ src/Chapter_1/Main.java
@@ -0,0 +1,50 @@
+package Chapter_1;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    private static final String TEXT = "Be strong, be fearless, be beautiful. "
+            + "And believe that anything is possible when you have the right "
+            + "people there to support you. ";
+
+    private static final String TEXT_CP = TEXT + "😍 I love 💕 you Ӝ so much 💕 😍 🎼🎼🎼!";
+
+    public static void main(String[] args) {
+        System.out.println("Input text: \n" + TEXT);
+        System.out.println("\nASCII or 16 bits Unicode characters (less than 65,535 (0xFFFF)) examples:\n");
+        System.out.println("HashMap based solution:");
+
+        long startTimeV1 = System.nanoTime();
+        Map<Character, Integer> duplicatesV1 = Strings.countDuplicateCharactersV1(TEXT);
+        displayExecutionTime(System.nanoTime() - startTimeV1);
+        System.out.println(Arrays.toString(duplicatesV1.entrySet().toArray()));
+
+        long startTimeV2 = System.nanoTime();
+        Map<Character, Long> duplicatesV2 = Strings.countDuplicateCharactersV2(TEXT);
+        displayExecutionTime(System.nanoTime() - startTimeV2);
+        System.out.println(Arrays.toString(duplicatesV2.entrySet().toArray()));
+
+        System.out.println("\n--------------------------------------\n");
+        System.out.println("Input text: \n" + TEXT_CP + "\n");
+        System.out.println("Including Unicode surrogate pairs examples:\n");
+        System.out.println("HashMap based solution:");
+
+        long startTimeV3 = System.nanoTime();
+        Map<String, Integer> duplicatesV3 = Strings.countDuplicateCharactersVCP1(TEXT_CP);
+        displayExecutionTime(System.nanoTime() - startTimeV3);
+        System.out.println(Arrays.toString(duplicatesV3.entrySet().toArray()));
+
+        long startTimeV4 = System.nanoTime();
+        Map<String, Long> duplicatesV4 = Strings.countDuplicateCharactersVCP2(TEXT_CP);
+        displayExecutionTime(System.nanoTime() - startTimeV4);
+        System.out.println(Arrays.toString(duplicatesV4.entrySet().toArray()));
+    }
+
+    private static void displayExecutionTime(long time) {
+        System.out.println("Execution time: " + time + " ns" + " (" +
+                TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS) + " ms)");
+    }
+}
---
+++ src/Chapter_1/Strings.java
@@ -0,0 +1,65 @@
+package Chapter_1;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Strings {
+
+    private Strings() {
+        throw new AssertionError("Can not be instantiated.");
+    }
+
+    public static @NotNull
+    Map<Character, Integer> countDuplicateCharactersV1(String str) {
+        if (str == null || str.isBlank()) {
+            return Collections.emptyMap();
+        }
+
+        Map<Character, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            result.compute(ch, (k, v) -> v == null ? 1 : ++v);
+        }
+        return result;
+    }
+
+    public static Map<Character, Long> countDuplicateCharactersV2(String str) {
+        Map<Character, Long> result = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        return result;
+    }
+
+    public static Map<String, Integer> countDuplicateCharactersVCP1(String str) {
+        Map<String, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            /*int cp = str.codePointAt(i);
+            String ch = String.valueOf(Character.toChars(cp));
+
+            if (Character.charCount(cp) == 2) {
+                i++;
+            }*/
+
+            String ch = String.valueOf(Character.toChars(str.codePointAt(i)));
+            if (i < str.length() - 1 && str.codePointCount(i, i + 2) == 1) {
+                ++i;
+            }
+            result.compute(ch, (k, v) -> v == null ? 1 : ++v);
+        }
+        return result;
+    }
+
+    public static Map<String, Long> countDuplicateCharactersVCP2(String str) {
+        Map<String, Long> result = str.codePoints()
+                .mapToObj(ch -> String.valueOf(Character.toChars(ch)))
+                .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
+        return result;
+    }
+}
